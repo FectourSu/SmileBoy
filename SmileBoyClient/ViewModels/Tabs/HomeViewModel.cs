7b77f3b89386c37834de9a6bb0853ca1574adc15
Dima Filin <dimongusta@gmail.com>
2021-04-04 14:04:21 +0300

    Add Home page & LiveCharts

--- SmileBoyClient/ViewModels/Tabs/HomeViewModel.cs
+++ SmileBoyClient/ViewModels/Tabs/HomeViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using LiveCharts;
+using LiveCharts.Configurations;
 using LiveCharts.Wpf;
 using LiveCharts.Wpf.Charts.Base;
+using System;
 using System.Drawing;
+using System.Linq;
 using System.Windows;
 
 namespace SmileBoyClient.ViewModels
@@ -17,19 +20,31 @@ namespace SmileBoyClient.ViewModels
                 {
                     Values = new ChartValues<double>
                     {
-                        1,
-                        5,
-                        7,
-                        8,
-                        5,
-                        2,
-                        8,
-                        6,
-                        2
+                        1023,
+                        2311,
+                        1111,
+                        2359,
+                        3005,
+                        4010,
+                        3100,
+                        2654,
+                        2913,
+                        3811,
+                        4607,
+                        3120,
+                        2230,
+                        3333,
+                        4225,
+                        4031,
+                        4229,
+                        4133,
+                        5012,
                     },
+                    LabelPoint = point => "$" + point.Y,
                     Fill = System.Windows.Media.Brushes.Transparent,
                 },
             };
         }
+        
     }
 }
